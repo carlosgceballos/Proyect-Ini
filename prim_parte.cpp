0cc8384ae2901cbf287d2e18ad92d77ccccf41bf
Carlos Ceballos <carlosgceballosq@gmail.com>
2024-11-13 23:06:59 -0400

    feat: estructura de datos para vehiculos, clientes y repuestos

--- prim_parte.cpp
+++ prim_parte.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
+struct Vehiculo{
+  string modelo, marca, placa, color, motor, ced_cliente, fecha_entrega;
+  int year, kilometraje;
+  bool rentado;
+  float precio_renta;
+};
+
+struct Cliente {
+    string cedula, nombre, apellido, email, direccion;
+    int cantidad_vehiculos_rentados;
+    bool activo;
+};
+
+struct Repuesto {
+    string modelo, marca, nombre, modelo_carro;
+    int anio_carro, existencias;
+    float precio;
+};
+
+//hacer funciones para almacenar y leer los respectivos datos
